515a1c396220abccb3047d09d97d9182bc29c5ec
aguiluxo91 <apuertadelaguila@gmail.com>
2022-04-24 05:18:56 +0200

    eslint changed in order to ignore it while building

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     REACT_APP_API_BASE_URL: 'http://localhost:3000/api',
-  }
+  },
+  eslint: { ignoreDuringBuilds: true }
 }
 
 module.exports = nextConfig
