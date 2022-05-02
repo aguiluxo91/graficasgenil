e39dd54131d5f002a841bc3f88d178973aa3ce89
aguiluxo91 <apuertadelaguila@gmail.com>
2022-05-02 21:46:22 +0200

    added express + cors. testing cors with express server

--- pages/api/mailer.js
+++ pages/api/mailer.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(3002)
 
 export default function (req, res) {
     const nodemailer = require('nodemailer');
